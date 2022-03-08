2532d3b6104c604ec935d9e1a2b4667382207d01
yunicoder <t.yabe.240@ms.saitama-u.ac.jp>
2022-03-09 02:07:50 +0900

    add output yaml

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import argparse
+import yaml
 
 from decide_team import DecideTeam
+
+def output_team_yaml(team):
+    team_dict = dict()
+    for i, group in enumerate(team):
+        team_dict[f"g{i+1}"] = group
+    with open("results/created_team.yaml", "w", encoding="utf-8") as file:
+        yaml.dump(team_dict, file, default_flow_style=False)
+    
 
 def main(args):
     decide_team = DecideTeam(args.group_num)
@@ -10,6 +19,8 @@ def main(args):
     print("------ decide team! ------")
     print(f"既に同じグループになったことのある組み合わせの数 : {not_first_cnt}")
     print(team)
+
+    output_team_yaml(team)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
