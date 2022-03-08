e80de822650224d9d31de075775a0750c2becc76
yunicoder <t.yabe.240@ms.saitama-u.ac.jp>
2022-03-09 02:31:38 +0900

    add comment and refactaring

--- decide_team.py
+++ decide_team.py
@@ -24,50 +24,51 @@ class DecideTeam:
         mem_num = len(member_list)
 
         ave_mem_num_per_group = mem_num / self.GROUP_NUM  # 1グループの平均メンバー数
-        plus_one_group_num = mem_num % self.GROUP_NUM  # 平均にならないグループの数
-        plus_one_group_list = random.sample(range(self.GROUP_NUM), plus_one_group_num)  # 平均にならないグループ
+        plus_one_group_num = mem_num % self.GROUP_NUM  # 平均より多く配置させないといけないグループ数
+        plus_one_group_list = random.sample(range(self.GROUP_NUM), plus_one_group_num)  # 平均より多く配置させないといけないグループ
         
-        # 学年ごとにグループに分ける
+        # 学年ごとにグループを決定する
         for grade_mem in members.values():
             grade_mem_num = len(grade_mem)
             random.shuffle(grade_mem)
 
-            grade_mem_num_per_group = int(len(grade_mem) / self.GROUP_NUM)
-            grade_plus_one_group_num = int(grade_mem_num % self.GROUP_NUM)
+            ave_grade_mem_num_per_group = int(len(grade_mem) / self.GROUP_NUM)  # 今回の学年における一グループあたりの平均メンバー数 
+            grade_plus_one_group_num = int(grade_mem_num % self.GROUP_NUM)  # 今回の学年における平均より多く配置させないといけないグループ数
 
             if grade_plus_one_group_num == 0:  # 全てのグループに平均数のメンバーを配置する場合
                 # 順番にグループに追加
                 cur_start_ind = 0
                 for i in range(self.GROUP_NUM):
-                    cur_end_ind = cur_start_ind + grade_mem_num_per_group
+                    cur_end_ind = cur_start_ind + ave_grade_mem_num_per_group
                     cur_team_members = grade_mem[cur_start_ind:cur_end_ind]
                     cur_team[i].extend(cur_team_members)
                     cur_start_ind = cur_end_ind
             else:
-                # 平均にならないグループを決定
+                # 多く配置グループを決定
                 cur_plus_one_groups = []  # 多く配置するグループ
-                cur_minus_one_groups = []  # 配置しないグループ
-                if len(plus_one_group_list) >= grade_plus_one_group_num:
-                    cur_plus_one_groups = random.sample(plus_one_group_list, grade_plus_one_group_num)
-                    for cur_plus_one_group in cur_plus_one_groups:
-                        plus_one_group_list.remove(cur_plus_one_group)  # 平均にならないグループリストから削除
-                else:
+
+                # (平均より多く配置するべき数)より(この学年で平均より多く配置するべき数)が上回ってしまっている時
+                if len(plus_one_group_list) < grade_plus_one_group_num:
                     cur_plus_one_groups = plus_one_group_list
                     plus_one_group_list.clear()
-                    cur_minus_one_groups = random.sample(range(self.GROUP_NUM), grade_plus_one_group_num-len(plus_one_group_list))
-                    for cur_minus_one_group in cur_minus_one_groups:
-                        plus_one_group_list.append(cur_minus_one_group)
+
+                    # 配置しない場所は次以降で多く配置しないといけない
+                    _cur_plus_one_groups = random.sample(range(self.GROUP_NUM), grade_plus_one_group_num-len(plus_one_group_list))
+                    plus_one_group_list.extend(_cur_plus_one_groups)
+                else:
+                    cur_plus_one_groups = random.sample(plus_one_group_list, grade_plus_one_group_num)
+                    for cur_plus_one_group in cur_plus_one_groups:
+                        plus_one_group_list.remove(cur_plus_one_group)  # これから多く配置するのでリストから消去
+
 
                 cur_start_ind = 0
                 for i in range(self.GROUP_NUM):
-                    if i in cur_plus_one_groups:  # 平均にならないグループは一人多く配置
-                        cur_end_ind = cur_start_ind + grade_mem_num_per_group + 1
-                    elif i in cur_minus_one_groups:
-                        if grade_mem_num_per_group == 0:
-                            continue
-                        cur_end_ind = cur_start_ind + grade_mem_num_per_group
+                    if i in cur_plus_one_groups:  # 多く配置するべきグループ
+                        cur_end_ind = cur_start_ind + ave_grade_mem_num_per_group + 1
                     else:
-                        cur_end_ind = cur_start_ind + grade_mem_num_per_group
+                        if ave_grade_mem_num_per_group == 0:
+                            continue  # 一人も配置されない場合
+                        cur_end_ind = cur_start_ind + ave_grade_mem_num_per_group
                     cur_team_members = grade_mem[cur_start_ind:cur_end_ind]
                     cur_team[i].extend(cur_team_members)
                     cur_start_ind = cur_end_ind
